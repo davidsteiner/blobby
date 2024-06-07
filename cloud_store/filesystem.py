73ef9adeb16444f568431cfd253f6f37203f1feb
David Steiner <david_j_steiner@yahoo.co.nz>
2024-06-07 15:11:51 +0200

    Add delete API to the storage

--- cloud_store/filesystem.py
+++ cloud_store/filesystem.py
@@ -4,6 +4,7 @@ from cloud_store.storage import Storage
 
 
 class FileSystemStorage(Storage):
+
     def __init__(self, root_dir: Path, *, create_missing_dirs: bool = False) -> None:
         self._root_dir = root_dir
         self._create_missing_dirs = create_missing_dirs
@@ -20,6 +21,9 @@ class FileSystemStorage(Storage):
 
     def get(self, path: Path) -> bytes:
         return self._full_path(path).read_bytes()
+
+    def delete(self, path: Path) -> None:
+        self._full_path(path).unlink()
 
     def _full_path(self, path: Path) -> Path:
         return self._root_dir / path
