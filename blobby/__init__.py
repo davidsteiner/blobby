91b1350f2c4badb577589f9351dcc52065eea958
David Steiner <david_j_steiner@yahoo.co.nz>
2024-06-11 19:50:39 -0400

    Make AWS and GCP dependencies optional

--- blobby/__init__.py
+++ blobby/__init__.py
@@ -1,14 +1,28 @@
 from blobby.storage import Storage
 
 from blobby.filesystem import FileSystemStorage
-from blobby.gcp import GoogleCloudStorage
+from blobby._gcp import GoogleCloudStorage
 from blobby.memory import MemoryStorage
-from blobby.s3 import S3Storage
 
 __all__ = [
     "FileSystemStorage",
-    "GoogleCloudStorage",
     "MemoryStorage",
-    "S3Storage",
     "Storage",
 ]
+
+
+try:
+    import boto3  # noqa: F401
+    from blobby._s3 import S3Storage  # noqa: F401
+
+    __all__.append("S3Storage")
+except ImportError:
+    pass
+
+try:
+    from google.cloud import storage  # noqa: F401
+    from blobby._gcp import GoogleCloudStorage  # noqa: F401
+
+    __all__.append("GoogleCloudStorage")
+except ImportError:
+    pass
